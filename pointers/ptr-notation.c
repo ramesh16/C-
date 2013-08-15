620507859802f19fab0d02524e54320775725a06
ramesh <ramesh.16786@gmail.com>
2013-08-15 17:33:49 +0530

    ptr notation file is changed by storing the array address in ptr

--- pointers/ptr-notation.c
+++ pointers/ptr-notation.c
@@ -3,10 +3,13 @@ int main()
 {
 	int arr[5] = {5, 10,15,20,25};
 	int i;
+	int *p = arr;
+	
 	for(i = 0; i < 5 ; i++)
 	{
-	printf("value of arr[%d] = %d\t",i,*(arr+i));
-	printf("Address of arr[%d] = %p\n",i,arr+i);
+
+	printf("Address of arr[%d] = %p = %p\n",i,p+i,&p[i]);
+	printf("value of arr[%d] = %d = %d\t",i,*(p+i),p[i]);
  	}
 	return 0;
 
